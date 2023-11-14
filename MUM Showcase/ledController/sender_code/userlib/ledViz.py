412fad04667e2e0719af8e7e6a0f211f3f882d5a
alxschmidt <45689749+alxschmidt@users.noreply.github.com>
2023-11-14 17:14:19 +0100

    Update ledViz.py
    Parser that transforms device state into visualization commands

--- MUM Showcase/ledController/sender_code/userlib/ledViz.py
+++ MUM Showcase/ledController/sender_code/userlib/ledViz.py
@@ -37,3 +37,79 @@ def visualize(ledCode):
         sendMsg(espnow.rightLedController,ledCode)
     else:
         print("Error: No ESP handles this ledCode.")
+
+
+# Helper functions so no parsing state > stream has to be done outside of this module
+# Parser for the TemperatureSensor:
+currentTempState = -1
+def visualizeTempState(newState):
+    newState = int(newState)
+    global currentTempState
+    
+    if (newState == 0):
+        pass
+    elif (newState == 1):
+        visualize(421)
+    elif (newState == 2):
+        visualize(461)
+        visualize(631)
+        visualize(321)
+    elif (newState == 3):
+        visualize(461)
+        visualize(631)
+        visualize(321)
+        visualize(311)
+    else:
+        print('Error: Received invalid state ${newState}. Aborting visualization thereof.')
+        return
+    
+    if (currentTempState == 1):
+        visualize(420)
+    elif (currentTempState == 2):
+        visualize(460)
+        visualize(630)
+        visualize(320)
+    elif (currentTempState == 3):
+        visualize(460)
+        visualize(630)
+        visualize(320)
+        visualize(310)
+
+    currentTempState = newState
+
+# Parser for the Cam:
+currentCamState = -1
+def visualizeCamState(newState):
+    newState = int(newState)
+    global currentCamState
+    
+    if (newState == 0):
+        pass
+    elif (newState == 1):
+        visualize(521)
+    elif (newState == 2):
+        visualize(561)
+        visualize(631)
+        visualize(321)
+    elif (newState == 3):
+        visualize(561)
+        visualize(631)
+        visualize(321)
+        visualize(311)
+    else:
+        print('Error: Received invalid state ${newState}. Aborting visualization thereof.')
+        return
+    
+    if (currentTempState == 1):
+        visualize(520)
+    elif (currentTempState == 2):
+        visualize(560)
+        visualize(630)
+        visualize(320)
+    elif (currentTempState == 3):
+        visualize(560)
+        visualize(630)
+        visualize(320)
+        visualize(310)
+
+    currentTempState = newState
