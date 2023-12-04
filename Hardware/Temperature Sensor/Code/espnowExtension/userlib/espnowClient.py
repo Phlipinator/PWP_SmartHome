a34f9ba63d5e24caaf5b210779434112b77b4b44
alxschmidt <45689749+alxschmidt@users.noreply.github.com>
2023-12-04 15:52:58 +0100

    espnow extension code

---
+++ Hardware/Temperature Sensor/Code/espnowExtension/userlib/espnowClient.py
@@ -0,0 +1,28 @@
+import espnow # type: ignore # (Tells Pylance to ignore this problem.)
+
+e = None
+
+leftLedController = b'\xc4\xde\xe2G0\xbc'   # MAC address of peer's wifi interface
+rightLedController = b'\xc4\xde\xe2G\x19\xa4'   # MAC address of peer's wifi interface
+
+def initESPNow():
+    global e
+    e = espnow.ESPNow()
+    e.active(True)
+
+def reinitESPNow():
+    global e
+    e.active(True)
+
+def deinitESPNow():
+    global e
+    e.active(False)
+
+def addPeer(peer):
+    e.add_peer(peer) # Must add_peer() before send()
+
+def getMessage():
+    return e.recv()
+
+def sendMessage(peer, msg):
+    e.send(peer, msg, True)
